47693a5e184bc232e8c6b12a873b3694b1fc78e3
Felipe300 <caracoloro@gmail.com>
2023-09-22 16:38:15 -0300

    chore: setup shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className=" text-3xl text-cyan-800">Hello World</h1>;
+  return (
+    <main>
+      <h1 className=" text-3xl text-cyan-800">Hello World</h1>
+      <Button>Click me</Button>
+    </main>
+  );
 }
